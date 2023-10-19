e5f8fa517886de1576c6c00202cdb0e0323adccd
“[Pradeep <[pradeepkumar.pk002@gmail.com]>
2023-10-19 14:07:52 +0530

    app crashed bug

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const app = require("./index");
 
 
+app.listen(process.env.PORT, () => {
+    mongoose
+      .connect(process.env.DB_CONNECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log('MongoDB Connected');
+        console.log(`App listening at http://localhost:${process.env.PORT}`);
+      })
+      .catch((err) => console.log(err));
+  });
+  
